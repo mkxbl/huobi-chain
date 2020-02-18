052b3a1569516049c7e4147b19e195b6318f7b11
zhounan <2627159529@qq.com>
2020-02-18 22:56:38 +0800

    add path

--- devtools/ci/windows-dependency.sh
+++ devtools/ci/windows-dependency.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -ev
 
-powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Bypass"
-powershell -Command "Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')"
+# powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Bypass"
+# powershell -Command "Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')"
+
+powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser";
+powershell -Command "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')";
+export PATH="$HOME/scoop/shims:$PATH;"; 
 
 powershell -Command "scoop install llvm"
 powershell -Command "scoop install msys2"
