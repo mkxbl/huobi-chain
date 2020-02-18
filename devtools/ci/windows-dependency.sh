8442e57298235555e23c937999ff29ea62032ea2
zhounan <2627159529@qq.com>
2020-02-18 20:35:25 +0800

    try ckb windows dependency

---
+++ devtools/ci/windows-dependency.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ev
+
+powershell -Command "Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')"
+
+set PATH=%PATH%;%USERPROFILE%\scoop\shims
+echo "##vso[task.setvariable variable=PATH;]%PATH%;%USERPROFILE%\scoop\shims"
+scoop help
+
+scoop install llvm
+scoop install msys2
+scoop install yasm
+
+rustc --version
+cargo --version
+clang --version
+yasm --version
+
+cargo build --release
