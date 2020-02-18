3735f55ca06edeb2f0b6604e15dab43389f4c816
zhounan <2627159529@qq.com>
2020-02-18 23:16:22 +0800

    try

--- devtools/ci/windows-dependency.sh
+++ devtools/ci/windows-dependency.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -ev
 
-# powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Bypass"
-# powershell -Command "Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')"
+powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Bypass"
+powershell -Command "Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')"
+set PATH=%PATH%;%USERPROFILE%\scoop\shims
+echo "##vso[task.setvariable variable=PATH;]%PATH%;%USERPROFILE%\scoop\shims"
+powershell -Command "scoop help"
 
-powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser";
-powershell -Command "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')";
-export PATH="$HOME/scoop/shims:$PATH;"; 
+# powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser";
+# powershell -Command "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')";
+# export PATH="$HOME/scoop/shims:$PATH;"; 
 
 powershell -Command "scoop install llvm"
 powershell -Command "scoop install msys2"
