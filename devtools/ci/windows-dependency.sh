37d73e69c70a518a546062a560c5669c5809f0a6
zhounan <2627159529@qq.com>
2020-02-18 22:49:51 +0800

    try scoop install

--- devtools/ci/windows-dependency.sh
+++ devtools/ci/windows-dependency.sh
@@ -4,9 +4,9 @@ set -ev
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Bypass"
 powershell -Command "Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')"
 
-scoop install llvm
-scoop install msys2
-scoop install yasm
+powershell -Command "scoop install llvm"
+powershell -Command "scoop install msys2"
+powershell -Command "scoop install yasm"
 
 rustc --version
 cargo --version
