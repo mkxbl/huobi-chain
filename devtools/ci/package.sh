dab9642151f8e79d0d87a08175167bb9669f6dbb
zhounan <2627159529@qq.com>
2020-02-18 15:30:23 +0800

    chore

--- devtools/ci/package.sh
+++ devtools/ci/package.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -ev
 
-echo "figure out where i am"
-echo $PWD
 mkdir package
 cp -r ./config ./package/
 if [ "$TRAVIS_OS_NAME" == "windows" ]
 then
     cp ./target/release/huobi-chain.exe ./package/
-    7z a huobi-chain-$TRAVIS_OS_NAME-$TRAVIS_TAG.zip package/
+    7z a huobi-chain-$TRAVIS_TAG-$TRAVIS_OS_NAME.zip package/
 else
     cp ./target/release/huobi-chain ./package/
-    tar zcvf huobi-chain-$TRAVIS_OS_NAME-$TRAVIS_TAG.tar.gz package/
+    tar zcvf huobi-chain-$TRAVIS_TAG-$TRAVIS_OS_NAME.tar.gz package/
 fi
