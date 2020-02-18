7184cd578c7abf5db1fc514dbf272bcf0aa7b324
zhounan <2627159529@qq.com>
2020-02-18 14:26:01 +0800

    test permisson

--- devtools/ci/package.sh
+++ devtools/ci/package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ev
 
 echo "figure out where i am"
 echo $PWD
@@ -8,8 +9,8 @@ cp -r ./config ./package/
 if [ "$TRAVIS_OS_NAME" == "windows" ]
 then
     cp ./target/release/huobi-chain.exe ./package/
-    7z a huobi-chain-$TRAVIS_OS_NAME.zip package/
+    7z a huobi-chain-$TRAVIS_OS_NAME-$TRAVIS_TAG.zip package/
 else
     cp ./target/release/huobi-chain ./package/
-    tar zcvf huobi-chain-$TRAVIS_OS_NAME.tar.gz package/
+    tar zcvf huobi-chain-$TRAVIS_OS_NAME-$TRAVIS_TAG.tar.gz package/
 fi
