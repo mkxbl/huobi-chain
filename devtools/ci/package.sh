8baf2d2b3d43cb0de5643a955d6c6e2036a47864
zhounan <2627159529@qq.com>
2020-02-18 14:09:57 +0800

    test tag

--- devtools/ci/package.sh
+++ devtools/ci/package.sh
@@ -2,8 +2,14 @@
 
 echo "figure out where i am"
 echo $PWD
-
-mkdir -p packages/$TRAVIS_OS_NAME
-cp -r ./config ./packages/$TRAVIS_OS_NAME/
-cp ./target/release/huobi-chain ./packages/$TRAVIS_OS_NAME/
-tar zcvf huobi-chain-$TRAVIS_OS_NAME.tar.gz packages/$TRAVIS_OS_NAME/
+cargo build --release
+mkdir package
+cp -r ./config ./package/
+if [ "$TRAVIS_OS_NAME" == "windows" ]
+then
+    cp ./target/release/huobi-chain.exe ./package/
+    7z a huobi-chain-$TRAVIS_OS_NAME.zip package/
+else
+    cp ./target/release/huobi-chain ./package/
+    tar zcvf huobi-chain-$TRAVIS_OS_NAME.tar.gz package/
+fi
